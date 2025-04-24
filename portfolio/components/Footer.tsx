7380ab5d1bccc140819e2c515e33f92cd368f5cb
DBlade02 <adityamogre0210@gmail.com>
2025-04-24 20:50:35 +0530

    add

--- portfolio/components/Footer.tsx
+++ portfolio/components/Footer.tsx
@@ -29,7 +29,7 @@ const Footer = () => {
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your hii.
         </p>
-        <a href="mailto:jagannathps2000@gmail.com">
+        <a href="adityamogre0210@gmail.com">
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}
--- portfolio/components/ui/BentoGrid.tsx
+++ portfolio/components/ui/BentoGrid.tsx
@@ -66,7 +66,7 @@ export const BentoGridItem = ({
   };
 
   const handleCopy = () => {
-    const text = "jagannathps2000@gmail.com";
+    const text = "adityamogre0210@gmail.com";
     navigator.clipboard.writeText(text);
     setCopied(true);
   };
